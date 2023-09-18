2d22e1f4588750ec0ae2a19f340795214be0bec1
Jaxcoder <codenamejason@outlook.com>
2023-09-18 19:29:10 -0400

    chore: add api call for pools

--- src/app/pool/page.tsx
+++ src/app/pool/page.tsx
@@ -1,6 +1,25 @@
-"use client";
 import Pool from "@/components/Pool/Pool";
+import { request, gql } from "graphql-request";
 
-export default function PoolHome() {
-  return <Pool />;
+export default async function PoolHome() {
+  // FIXME: THE API DOES NOT RETURN THE POOL NAME AND STATUS
+  const query = gql`
+    {
+      pools {
+        poolId
+        chainId
+        strategy
+        token
+        amount
+        profile {
+          owner
+          metadataPointer
+        }
+      }
+    }
+  `;
+  const data: any = await request("http://localhost:5555/graphql", query);
+  const { pools } = data;
+
+  return <Pool data={pools}/>;
 }
---
+++ src/utils/utils.ts
@@ -0,0 +1,20 @@
+import { getNetworks } from "../api/networks";
+import { ethers } from "ethers";
+
+const networks = getNetworks();
+
+export const convertChainIdToNetworkName = (chainId: number) => {
+  return `${networks[chainId].name} (${chainId})`;
+};
+
+export function classNames(...classes: any[]) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export function formatAmount(amount: number) {
+  return ethers.formatEther(amount);
+}
+
+export function shortenPoolName(name: string) {
+  return name.length > 10 ? `${name.slice(0, 10)}...` : name;
+}
