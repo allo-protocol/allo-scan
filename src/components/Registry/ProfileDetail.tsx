f0189c29cf25f72f78c79f3e2694bc33e8322a42
0xKurt <Kurt@Merbeth.io>
2023-09-19 18:41:21 +0200

    last things

--- src/components/Registry/ProfileDetail.tsx
+++ src/components/Registry/ProfileDetail.tsx
@@ -2,6 +2,10 @@
 import { convertChainIdToNetworkName } from "@/utils/utils";
 import { AddressResponsive } from "../Address";
 import { TProfileDetails } from "./types";
+import { MetadataProtocol } from "@/types/types";
+import { TbExternalLink } from "react-icons/tb";
+import JsonView from "@uiw/react-json-view";
+
 
 const ProfileDetail = ({
   profile,
@@ -19,14 +23,14 @@ const ProfileDetail = ({
           {profile.name}
         </h3>
         <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500 font-mono">
-          {profile.id}
+          {profile.profileId}
         </p>
       </div>
       <div className="mt-6 border-t border-gray-100">
         <dl className="divide-y divide-gray-100">
           <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
             <dt className="text-sm font-medium leading-6 text-gray-900">
-              Chain
+              Network
             </dt>
             <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
               {convertChainIdToNetworkName(profile.chainId)}
@@ -57,7 +61,7 @@ const ProfileDetail = ({
             </dt>
             <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
               <AddressResponsive
-                address={profile.sender}
+                address={profile.creator}
                 chainId={profile.chainId}
               />
             </dd>
@@ -96,7 +100,28 @@ const ProfileDetail = ({
               </ul>
             </dd>
           </div>
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              Metadata ({MetadataProtocol[profile.metadata.protocol]}){" "}
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              <div className="flex flex-row items-center">
+                {profile.metadata.pointer}
+                <a
+                  className="ml-2"
+                  // data-tip="view on explorer"
+                  target="_blank"
+                  href={"https://ipfs.io/ipfs/" + profile.metadata.pointer}
+                >
+                  <TbExternalLink />
+                </a>
+              </div>
+            </dd>
+          </div>
         </dl>
+        <div className="pb-6">
+          <JsonView value={metadataObj} shortenTextAfterLength={120} collapsed={2} />
+        </div>
       </div>
     </div>
   );
