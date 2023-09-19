4e29829c7199fd2a1de415f98357fbaa69d08723
Jaxcoder <codenamejason@outlook.com>
2023-09-19 10:30:39 -0400

    chore: extract queries

--- src/app/pool/page.tsx
+++ src/app/pool/page.tsx
@@ -1,28 +1,14 @@
 import { Loading } from "@/components/Loading";
 import Pool from "@/components/Pool/Pool";
+import { getPoolDataQuery } from "@/utils/query";
 import { graphqlEndpoint } from "@/utils/utils";
-import { request, gql } from "graphql-request";
+import { request } from "graphql-request";
 import { Suspense } from "react";
 
 export default async function PoolHome() {
   // FIXME: THE API DOES NOT RETURN THE POOL NAME AND STATUS
-  const query = gql`
-    {
-      pools {
-        poolId
-        chainId
-        strategy
-        token
-        amount
-        profile {
-          name
-          owner
-          metadataPointer
-        }
-      }
-    }
-  `;
-  const data: any = await request(graphqlEndpoint, query);
+
+  const data: any = await request(graphqlEndpoint, getPoolDataQuery);
   const { pools } = data;
 
   return (
