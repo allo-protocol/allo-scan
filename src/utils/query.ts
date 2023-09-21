2887eaebdcf0cfdc775919f802ca74e3714ff851
Jaxcoder <codenamejason@outlook.com>
2023-09-21 15:59:36 -0400

    chore: add latest queries/add enum for orderBy

--- src/utils/query.ts
+++ src/utils/query.ts
@@ -1,7 +1,11 @@
 import { gql } from "graphql-request";
 
+/** Endpont for graphql queries
+ *  todo: make this configurable and move to .env
+ */
 export const graphqlEndpoint = "http://localhost:5555/graphql";
 
+/** Returns all the pools */
 export const getPoolDataQuery = gql`
   {
     pools {
@@ -19,28 +23,30 @@ export const getPoolDataQuery = gql`
   }
 `;
 
+/** Returns pool information for a given network and pool ID */
 export const getPoolDetailDataQuery = gql`
   query GetPoolDetails($chainId: String!, $poolId: String!) {
     pool(chainId: $chainId, poolId: $poolId) {
-        poolId
-        chainId
-        token
-        amount
-        strategy
-        metadataProtocol
-        metadataPointer
-        tokenMetadata
-        profile {
-          profileId
-          name
-          owner
-          anchor
-          creator
-        }
+      poolId
+      chainId
+      token
+      amount
+      strategy
+      metadataProtocol
+      metadataPointer
+      tokenMetadata
+      profile {
+        profileId
+        name
+        owner
+        anchor
+        creator
       }
     }
+  }
 `;
 
+/** Returns all of the profiles */
 export const getProfileDataQuery = gql`
   {
     profiles {
@@ -53,6 +59,30 @@ export const getProfileDataQuery = gql`
   }
 `;
 
+/** Function that finds all active role_account records for a given role */
+export const getActiveProfileDataQuery = gql`
+  query GetAllActiveProfiles {
+    profiles {
+      profileId
+      anchor
+      name
+      chainId
+      creator
+      createdAt
+      metadataPointer
+      metadataProtocol
+      nonce
+      owner
+      role {
+        activeAccounts {
+          accountId
+        }
+      }
+    }
+  }
+`;
+
+/** Returns the profile information for a given network and profile ID */
 export const getProfileDetailDataQuery = gql`
   query getProfileDetails($chainId: String!, $profileId: String!) {
     profile(chainId: $chainId, profileId: $profileId) {
@@ -73,7 +103,6 @@ export const getProfileDetailDataQuery = gql`
           creator
         }
       }
-
       profileId
       anchor
       name
@@ -84,7 +113,6 @@ export const getProfileDetailDataQuery = gql`
       metadataProtocol
       nonce
       owner
-
       role {
         roleAccounts {
           accountId
@@ -94,8 +122,9 @@ export const getProfileDetailDataQuery = gql`
   }
 `;
 
+/** Return Allo information for a given network */
 export const getAlloStatsQuery = gql`
-  query AlloStats ($chainId: String!) {
+  query AlloStats($chainId: String!) {
     allo(chainId: $chainId) {
       registry
       feePercentage
@@ -107,6 +136,7 @@ export const getAlloStatsQuery = gql`
   }
 `;
 
+/** Return all Allo transactions */
 export const getAlloTransactions = gql`
   {
     alloTransactions {
@@ -120,6 +150,64 @@ export const getAlloTransactions = gql`
       blockNumber
       blockTimestamp
       chainId
+    }
+  }
+`;
+
+/** Function that finds all Allo transactions sent from a particular address (and network) */
+export const getAlloTransactionsFromAddress = gql`
+  query GetAlloTransactionsByAddress($chainId: String!, $address: String!) {
+    alloTransactionsFrom(chainId: $chainId, fromAddress: $address) {
+      hash
+      fromAddress
+      toAddress
+      functionName
+      functionArgs
+      blockHash
+      blockNumber
+      blockTimestamp
+    }
+  }
+`;
+
+/** Function that finds all Allo transactions sent to a particular address (and network) */
+export const getAlloTransactionsToAddress = gql`
+  query GetAlloTransactionsByAddress($chainId: String!, $address: String!) {
+    {
+      alloTransactionsTo(
+        chainId: $chainId
+        toAddress: $address
+      ) {
+        hash
+        fromAddress
+        toAddress
+        functionName
+        functionArgs
+        blockHash
+        blockNumber
+        blockTimestamp
+      }
+    }
+  }
+`;
+
+/** Function that finds all Allo transactions sent to or from a particular address (and network) */
+/** NOTE: The orderBy enum only has one so far */
+export const getAlloTransactionsInvolved = gql`
+  query GetAlloTransactionsInvolving($chainId: String!, $address: String!, $orderBy: [OrderBy!]!) {
+    alloTransactionsInvolving(
+      chainId: $chainId
+      address: $address
+      orderBy: [OrderBy.BLOCK_TIMESTAMP_DESC]
+    ) {
+      hash
+      fromAddress
+      toAddress
+      functionName
+      functionArgs
+      blockHash
+      blockNumber
+      blockTimestamp
     }
   }
 `;
